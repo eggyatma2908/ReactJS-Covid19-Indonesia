5eed15d3217b02d83ad45314d4579ed51953cffe
eggyatma2908 <eggyatmariansyah@gmail.com>
2021-01-18 22:56:35 +0700

    Version 1.0.0

--- src/components/base/BurgerMenu.js
+++ src/components/base/BurgerMenu.js
@@ -5,11 +5,13 @@ import { slide as Menu } from 'react-burger-menu'
 export default class BurgerMenu extends Component {
     render() {
         return (
+            <div className="hamburgermenu">
                 <Menu>
                     <Link to="/" id="home" className="menu-item">Dashboard</Link>
                     <Link to="/contact" id="about" className="menu-item">Contact</Link>
                     <Link to="/api" id="contact" className="menu-item">API for Developers</Link>
                 </Menu>
+            </div>
         )
     }
 }
